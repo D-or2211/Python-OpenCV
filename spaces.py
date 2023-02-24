5cd2b3c8c565ea3e9ef6a027a834fb84681e6bc1
Demian <63124612+D-or2211@users.noreply.github.com>
2023-02-24 11:44:27 -0600

    Update 1
    Segundo Cambio

--- spaces.py
+++ spaces.py
@@ -12,7 +12,7 @@ img = rescaleFrame(img)
 
 # BGR to Grayscale
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('Gray', gray)
+#cv.imshow('Gray', gray)
 
 # BGR to HSV
 hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
@@ -20,11 +20,15 @@ cv.imshow('hsv', hsv)
 
 # BGR to LAB
 lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
-cv.imshow('lab', lab)
+#cv.imshow('lab', lab)
 
 # BGR to RGB
 rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-cv.imshow('RGB', rgb)
+#cv.imshow('RGB', rgb)
+
+# HSV to BGR
+hsv_bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+cv.imshow('HSV --> BGR', hsv_bgr)
 
 cv.imshow('Gatito', img)
 cv.waitKey(0)
